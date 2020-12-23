4c8c0937988928b07074e68a2478c5008fd80085
unknown <nicolas.baudvin@gmail.com>
2020-12-23 16:26:50 +0100

    Implement Menu

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,11 +1,25 @@
 import Board from '../Board';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './style.scss';
+import Menu from '../Menu';
 
 const App = () => {
   return (
-    <div className="App">
-      <Board />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Menu />
+          </Route>
+          <Route exact path="/game/vsIA">
+            <Board isOnline={false} />
+          </Route>
+          <Route exact path="/game/vsFriends">
+            <Board isOnline />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
