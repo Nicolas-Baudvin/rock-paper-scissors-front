46dffc4a266bd784eaf5cf930fdf910eed7cca7f
unknown <nicolas.baudvin@gmail.com>
2020-12-23 13:11:19 +0100

    place Rock Paper Scissors buttons

--- src/Components/Board/index.js
+++ src/Components/Board/index.js
@@ -1,18 +1,21 @@
 import './style.scss';
+import Header from './Header';
 
 const Board = () => {
     return <main className="board">
-        <header className="board-header">
-            <div className="board-header-title">
-                <img src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="Rock Paper Scissors" />
+        <Header score={0} />
+        <div className="board-game">
+            <img className="board-game-pic" src={`${process.env.PUBLIC_URL}/img/bg-triangle.svg`} alt="Board" />
+            <div className="board-game-rock">
+                <img src={`${process.env.PUBLIC_URL}/img/icon-rock.svg`} alt="" />
             </div>
-            <div className="board-header-score">
-                <div className="board-header-score__item">
-                    <p className="board-header-score__item--subtitle">score</p>
-                    <p className="board-header-score__item--nbr">0</p>
-                </div>
+            <div className="board-game-paper">
+                <img src={`${process.env.PUBLIC_URL}/img/icon-paper.svg`} alt="" />
             </div>
-        </header>
+            <div className="board-game-scissors">
+                <img src={`${process.env.PUBLIC_URL}/img/icon-scissors.svg`} alt="" />
+            </div>
+        </div>
     </main>
 };
 
