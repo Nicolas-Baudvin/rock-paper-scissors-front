a8186a298d748ecd5b1bc0f39e7e85df9d6510d7
unknown <nicolas.baudvin@gmail.com>
2020-12-24 11:07:15 +0100

    Add a Menu button

--- src/Components/Board/Footer/index.js
+++ src/Components/Board/Footer/index.js
@@ -1,7 +1,18 @@
+import { useHistory } from "react-router-dom";
+
 const Footer = ({ handleClickRules }) => {
+    const history = useHistory();
+
+    const handleClickBackToMenu = () => {
+        history.push("/");
+    };
+
     return <div className="footer">
-        <button onClick={handleClickRules} className="rules">
+        <button onClick={handleClickRules} className="footer-button">
             Rules
+        </button>
+        <button onClick={handleClickBackToMenu} className="footer-button">
+            Menu
         </button>
     </div>
 };
