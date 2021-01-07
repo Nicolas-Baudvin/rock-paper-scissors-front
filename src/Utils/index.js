54e8c6417d83f9f219d60e559b5c937a3a505fda
unknown <nicolas.baudvin@gmail.com>
2021-01-07 13:12:28 +0100

    add Engines

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,4 +1,11 @@
 import shotTypeObject from './shotTypeObject';
 import winRulesObject from './winRulesObject';
+import getStringFromLocalStorage from './getStringFromLocalStorage';
+import getObjectFromLocalStorage from './getObjectFromLocalStorage';
 
-export { shotTypeObject, winRulesObject };
+export {
+    shotTypeObject,
+    winRulesObject,
+    getStringFromLocalStorage,
+    getObjectFromLocalStorage
+};
