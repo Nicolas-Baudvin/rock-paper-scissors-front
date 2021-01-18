cd5efb00c5b1effcf7c0fcc5ba70d6df0220e580
unknown <nicolas.baudvin@gmail.com>
2021-01-18 13:39:45 +0100

    add classname to p with winner textr

--- src/Components/Board/OnlineResult/FinalResult.tsx
+++ src/Components/Board/OnlineResult/FinalResult.tsx
@@ -9,7 +9,9 @@ const FinalResult = ({ handleClickReset }: Props) => {
   const { winner } = useSelector((state: RootState) => state.socket);
   return (
     <div className="board-result-final">
-      <p>{winner !== "equal" ? `Winner is ${winner}` : "Equality !"}</p>
+      <p className="board-result-final-text">
+        {winner !== "equal" ? `Winner is ${winner}` : "Equality !"}
+      </p>
       <button onClick={handleClickReset} className="board-result-final-restart">
         Play Again
       </button>
